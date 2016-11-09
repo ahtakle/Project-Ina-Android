78c2272083db320ab0f5c6a93e57d5c543f6ecbf
zrs <zrskemp@gmail.com>
2016-11-09 02:31:59 -0500

    First example demo

--- app/src/main/java/com/projectina/ina/Home.java
+++ app/src/main/java/com/projectina/ina/Home.java
@@ -1,7 +1,12 @@
 package com.projectina.ina;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import static android.provider.AlarmClock.EXTRA_MESSAGE;
 
 public class Home extends AppCompatActivity {
 
@@ -10,4 +15,15 @@ public class Home extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
     }
+
+
+    /** Called when the user clicks the Send button */
+    public void dispatchTrimesterIntent(View view) {
+        Intent intent = new Intent(this, Trimesters.class);
+        /*EditText editText = (EditText) findViewById(R.id.edit_message);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);*/
+        startActivity(intent);
+    }
+
 }
