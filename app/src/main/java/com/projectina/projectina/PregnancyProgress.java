aad4a43c1231c352648b3799b779c41b34edeb7c
schoi1826 <schoi1826@gmail.com>
2019-11-06 11:15:25 -0500

    Update pregnancy progress checklists

--- app/src/main/java/com/projectina/projectina/PregnancyProgress.java
+++ app/src/main/java/com/projectina/projectina/PregnancyProgress.java
@@ -211,111 +211,160 @@ public class PregnancyProgress extends AppCompatActivity {
         HashMap<String, List<String>> expandableListDetail = new HashMap<String, List<String>>();
 
         trim1 = new ArrayList<String>();
-        trim1.add("Start taking a prenatal vitamin (make sure Folic Acid is included)");
-        trim1.add("Choose a healthcare provider");
-        trim1.add("Investigate what your health insurance will cover for prenatal costs and your new baby\n\u2022 Apply for Medicaid if you need to");
-        trim1.add("Make a prenatal appointment\n\u2022 Consult your provider about medications you’re taking");
-        trim1.add("Make sure your activities of daily life are pregnancy-safe");
-        trim1.add("Research prenatal exercises and talk to your doctor about which ones are safe");
-        trim1.add("Get relief from morning sickness");
-        trim1.add("Consider your options for prenatal testing");
-        trim1.add("Pick up pregnancy books");
-        trim1.add("Learn the signs of pregnancy problems");
-        trim1.add("Think about when and how you’ll announce your pregnancy");
-        trim1.add("Follow your baby’s development");
-        trim1.add("Start taking belly photos");
+        trim1.add("Choose a health care provider");
+        trim1.add("Make a prenatal appointment");
+        trim1.add("Investigate what your health insurance will cover for prenatal costs and your " +
+                "new baby");
+        trim1.add("Apply for Medicaid if you need to");
+        trim1.add("Discuss with your health care provider: Should I start taking a prenatal " +
+                "vitamin with Folic Acid?");
+        trim1.add("Discuss with your health care provider: Are my current medications safe to " +
+                "take during pregnancy(make sure to bring all medications to your first " +
+                "appointment)?");
+        trim1.add("Discuss with your health care provider: Are there over-the-counter medications" +
+                " or other substances that I should avoid during pregnancy?");
+        trim1.add("Discuss with your health care provider: What activities of daily life are " +
+                "pregnancy-safe during my fist trimester and are their activities I should avoid?");
+        trim1.add("Discuss with your health care provider: Are there prenatal exercises that will" +
+                " be beneficial for me during my first trimester?");
+        trim1.add("Discuss with your health care provider: How can I get relief from morning " +
+                "sickness?");
+        trim1.add("Discuss with your health care provider: What are my options for prenatal " +
+                "testing?");
+        trim1.add("Discuss with your health care provider: What are the signs of pregnancy " +
+                "problems and what should I do if I experience them?");
+        trim1.add("Discuss with your health care provider: What foods are healthy and what foods " +
+                "are not healthy during pregnancy?");
+        trim1.add("Discuss with your health care provider: When should I start thinking about a " +
+                "birth plan and what are my options?");
+        trim1.add("Discuss with your health care provider: Is it safe for me to be vaccinated " +
+                "during pregnancy?");
         trim1.add("Stock your kitchen with healthy food");
+        trim1.add("Think about when and how you'll announce your pregnancy");
+        trim1.add("Pick up pregnancy books");
+        trim1.add("Follow your baby's development");
+        trim1.add("Start taking belly photos");
         trim1.add("Start a daily ritual to connect with your baby");
         trim1.add("Buy some new bras and underwear");
-        trim1.add("Talk to your partner about parenting");
-        trim1.add("Start making a “Baby Budget”");
-        trim1.add("Research your birth plan");
-        trim1.add("If you are working, research your workplace’s maternity leave policies");
-        trim1.add("Learn about vaccines");
+        trim1.add("Initiate discussions with your partner about parenting");
+        trim1.add("Start making a pre and post birth \"baby budget\"");
+        trim1.add("Start researching for your birth plan");
+        trim1.add("If you are working, research your workplace's maternity leave policies");
 
         trim2 = new ArrayList<String>();
-        trim2.add("Learn about second-semester prenatal visits and tests");
-        trim2.add("Find a prenatal exercise class");
+        trim2.add("Learn about second trimester prenatal visits and tests");
+        trim2.add("Make your second trimester prenatal appointments");
+        trim2.add("Discuss with your health care provider: What activities of daily life are " +
+                "pregnancy-safe during my second trimester?");
+        trim2.add("Discuss with your health care provider: Are there prenatal exercises that will" +
+                " be beneficial for me during my second trimester?");
+        trim2.add("Discuss with your health care provider: What prenatal tests do I need to " +
+                "schedule during this time?");
+        trim2.add("Discuss with your health care provider: Should I make any changes to my daily " +
+                "routines, sleep habits, medications, and foods?");
+        trim2.add("Discuss with your health care provider: Do I need to worry about having sex " +
+                "with my partner?");
+        trim2.add("Discuss with your health care provider: When and how will I find out the sex " +
+                "of my baby?");
+        trim2.add("Discuss with your health care provider: What if I don't want to know the sex " +
+                "of my baby?");
+        trim2.add("Make dental appointment to get your teeth cleaned");
+        trim2.add("Find a prenatal exercise class, if approved by your health care provider");
         trim2.add("Start shopping for maternity clothes");
-        trim2.add("Decide whether to hire a professional labor coach (doula/midwife)");
-        trim2.add("Narrow your baby names list");
-        trim2.add("Plan some adult time");
+        trim2.add("Start thinking about baby names");
         trim2.add("Start moisturizing your belly");
         trim2.add("Decide whether you want to find out the sex of your baby");
         trim2.add("Look into childbirth classes");
-        trim2.add("Flesh out your “Baby Budget”");
+        trim2.add("Flesh out your \"baby budget\"");
+        trim2.add("Talk to your employer about maternity leave");
         trim2.add("Prepare your older children");
         trim2.add("Prepare your pets");
         trim2.add("Start your childcare search");
-        trim2.add("Get your teeth cleaned");
         trim2.add("Celebrate your halfway point");
+        trim2.add("Plan some adult time and consider a \"babymoon\"");
         trim2.add("Start sleeping on your side");
         trim2.add("Start doing Kegel exercises");
-        trim2.add("Talk to your employer about maternity leave");
-        trim2.add("Consider a “babymoon”");
-        trim2.add("Think about your baby shower\n\u2022 Create a baby registry");
+        trim2.add("Think about your baby shower and create a baby registry");
         trim2.add("Write a letter to your baby");
-        trim2.add("Tackle your home improvement projects");
-        trim2.add("Think about who will care for your baby (daycare, nanny, family member)");
-        trim2.add("Make dental appointment");
-        trim2.add("If you want to, find out the gender of your baby");
-        trim2.add("Research and order nursery furniture, strollers, car seats, and other baby supplies");
-
+        trim2.add("Ready your baby's room");
+        trim2.add("Think about who will care for your baby(daycare, nanny, family member)");
+        trim2.add("If you want, find out the gender of your baby");
+        trim2.add("Decide whether to hire a professional labor coach(doula/midwife)");
+        trim2.add("Research and order nursery furniture, strollers, car seats, and other baby " +
+                "supplies(stay within your \"baby budget\")");
 
         trim3 = new ArrayList<String>();
+        trim3.add("Learn about third trimester prenatal visits and tests");
+        trim3.add("Make your third trimester prenatal appointments");
+        trim3.add("Discuss with your health care provider: What activities of daily life are " +
+                "pregnancy-safe during my third trimester?");
+        trim3.add("Discuss with your health care provider: Are there restrictions on travel " +
+                "during this time?");
+        trim3.add("Discuss with your health care provider: Are there prenatal exercises that will" +
+                " be beneficial for me during my third trimester?");
+        trim3.add("Discuss with your health care provider: What prenatal tests do I need to " +
+                "schedule during this time?");
+        trim3.add("Discuss with your health care provider: Should I make any changes to my daily " +
+                "routines, sleep habits, medications, and foods?");
+        trim3.add("Discuss with your health care provider: What should I expect as my delivery " +
+                "time draws near?");
+        trim3.add("Discuss with your health care provider: How will I know if I'm really in " +
+                "labor?");
+        trim3.add("Discuss with your health care provider: What should I know about " +
+                "late-pregnancy complications?");
+        trim3.add("Discuss with your health care provider: What should I expect after giving " +
+                "birth?");
         trim3.add("Finalize your birth plan");
-        trim3.add("Keep track of your baby’s movements");
-        trim3.add("Learn about third-trimester prenatal visits and tests");
+        trim3.add("Keep track of your baby's movements");
         trim3.add("Consider more classes");
         trim3.add("Prepare for breastfeeding");
         trim3.add("Choose a pediatrician");
-        trim3.add("Think about big decisions (like circumcision if your baby is a boy, religious ceremonies, etc.)");
-        trim3.add("Set up a safe place for your baby to sleep\n\u2022 Wash your baby’s clothing and bedding");
+        trim3.add("Think about big decisions(like circumcision if your baby is a boy, religious " +
+                "ceremonies, etc.)");
+        trim3.add("Set up a safe place for your baby to sleep");
+        trim3.add("Wash your baby's clothing and bedding");
         trim3.add("Learn about the stages of labor and coping with labor pain");
-        trim3.add("Start thinking about and contacting “helpers” (family, friends, daycare, etc)");
-        trim3.add("Finalize your “Baby Budget”\n\u2022 Consider the most financially stressful baby costs and how you can save");
+        trim3.add("Learn about how your body will look and feel after birth");
+        trim3.add("Start thinking about and contacting \"helpers\"(family, friends, daycare, etc" +
+                ".)");
+        trim3.add("Finalize your post-delivery \"baby budget\"");
+        trim3.add("Consider the most financially stressful baby costs and plan for how you might " +
+                "deal with them");
+        trim3.add("Consider how you might be able to save on recurring expenses");
         trim3.add("Read up on baby care");
-        trim3.add("Pack your bag for the hospital or birth center");
-        trim3.add("Clean your house");
-        trim3.add("Stock up on household and personal supplies");
-        trim3.add("Make food for after your baby’s born");
-        trim3.add("Install your baby’s car seat");
+        trim3.add("Pack your bags for the hospital or birth center");
         trim3.add("Tour your hospital or birth center");
         trim3.add("Make a plan for when labor starts");
-        trim3.add("Research and look out for late-pregnancy complications");
-        trim3.add("Think about baby names");
-        trim3.add("Cope with late pregnancy jitters");
-        trim3.add("Learn about how your body will look and feel after birth");
+        trim3.add("Look out for late-pregnancy complications");
+        trim3.add("Look out for signs of false labor");
+        trim3.add("Clean your house in preparation for the baby's arrival");
+        trim3.add("Stock up on household and personal supplies");
         trim3.add("Stock up on light entertainment");
-        trim3.add("Research and look out for false labor signs");
-        trim3.add("Decide who will be in your delivery room");
-        trim3.add("Get tested for gestational diabetes");
-        trim3.add("Pre-register at your hospital");
-
+        trim3.add("Plan meals and make food for after your baby's born");
+        trim3.add("Install your infant car seat");
+        trim3.add("Finalize your list of baby names");
+        trim3.add("Cope with late-pregnancy jitters");
 
         daily = new ArrayList<String>();
-        daily.add("Drink water");
-        daily.add("Eat healthy");
-        daily.add("No smoking");
-        daily.add("No drinking alcohol");
-        daily.add("Not a lot of caffeine");
-        daily.add("Avoid hazardous foods (could have bacteria, parasites, or toxins like raw fish)");
-        daily.add("Stretch");
-        daily.add("Sneak in a pregnancy power nap");
-        daily.add("Try a relaxation technique");
-        daily.add("Take a quick walk");
-        daily.add("Eat a pregnancy superfood");
-        daily.add("Write down your pregnancy memories");
-        daily.add("Track your weight gain");
-        daily.add("Do something nice for yourself");
-        daily.add("Jot down your crazy pregnancy dreams");
-        daily.add("Check in with a friend");
-        daily.add("Know the signs of a pregnancy problem");
-        daily.add("Take belly photos");
-        daily.add("Have sex if you feel up to it");
-        daily.add("Go to bed early");
-        daily.add("Avoid unsafe activities");
-
+        daily.add("Daily do's: Drink water");
+        daily.add("Daily do's: Eat healthy");
+        daily.add("Daily do's: Get a good night's sleep");
+        daily.add("Daily do's: Check in with a friend");
+        daily.add("Daily do's: Know the signs of a pregnancy problem");
+        daily.add("Daily do's: Limit caffeine");
+        daily.add("Daily do's: Avoid dangerous food with bacteria, parasites, or toxins like raw " +
+                "fish");
+        daily.add("Daily do's: Track your weight gain");
+        daily.add("Daily don'ts: No smoking");
+        daily.add("Daily don'ts: No drinking alcohol");
+        daily.add("For fun: Stretch");
+        daily.add("For fun: Sneak in a pregnancy power nap");
+        daily.add("For fun: Try a relaxation technique");
+        daily.add("For fun: Take a quick walk");
+        daily.add("For fun: Write down your pregnancy memories");
+        daily.add("For fun: Do something nice for yourself");
+        daily.add("For fun: Jot down your crazy pregnancy dreams");
+        daily.add("For fun: Take belly photos");
 
         expandableListDetail.put("Trimester 1", trim1);
         expandableListDetail.put("Trimester 2", trim2);
